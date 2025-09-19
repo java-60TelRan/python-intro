7468d7b4adb33f36851561c9e8f480654f43e0fe
Yuragranovsky <yuragranovsky@gmail.com>
2025-09-19 19:25:01 +0300

    HW #21 solution

---
+++ README.md
@@ -0,0 +1,3 @@
+# HW 21 Binary search update (function bSearchSortedList)
+## Return index of first occurrence for found out a searched number
+## Return negative value designating index of inserting into sorted list for non found out a searched number
--- main.py
+++ main.py
@@ -4,15 +4,20 @@ def bSearchSortedList(lst: list[int], num: int) -> int:
     # returns index of the number if exists, otherwise -1
     left: int = 0
     right: int = len(lst) - 1
-    middle: int = (left + right) // 2
-    while left <= right and lst[middle] != num :
-        if num < lst[middle] : 
+    middle: int = -1
+    while left <= right :
+        middle = (right + left) // 2
+        if num <= lst[middle] : 
             right = middle - 1
         else :
             left = middle + 1
-        middle = (right + left) // 2
-    return middle if left <= right else -1       
+        
+    return left if len(lst) > left and lst[left] == num  else -(left + 1)      
     
 
-numbers: list[int] = [1, 5, 20, 20, 20, 20, 20,30, 100]
-print (bSearchSortedList(numbers, 4))
+numbers: list[int] = [1, 1, 5, 20, 20, 20, 20, 20, 30, 100]
+print (bSearchSortedList(numbers, 20)) # output 3 (first occurrence)
+print (bSearchSortedList(numbers, 1)) # output 0 (first occurrence)
+print (bSearchSortedList(numbers, 200)) # output -11 (isertion position after last number)
+print (bSearchSortedList(numbers, -10)) # output -1 (isertion position before first number)
+print (bSearchSortedList(numbers, 10)) # output -4 (isertion position before to keep sorted)
