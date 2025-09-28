f9f65daf5b5494c601cb6d0daeea2e13b7591e58
Yuragranovsky <yuragranovsky@gmail.com>
2025-09-28 19:06:55 +0300

    range in unsorted

--- main.py
+++ main.py
@@ -1,44 +1,14 @@
+numbers:list[int] = []
+numbers.append(30)
+numbers.append(20)
+numbers.append(50)
+numbers.append(3)
 
-def isSumTwo(numbers: list[int], sum: int):
-    helper: set[int] = set()
-    indx: int = 0
-    res: bool = False
-    length: int = len(numbers)
-    while indx < length and not res:
-        if sum - numbers[indx] in helper:
-            res = True
-        else:
-            helper.add(numbers[indx])
-            indx += 1
-    return res
-
-
-def maxNegativeRepr(numbers: list[int]):
-    res: int = 0
-    helper: set[int] = set()
-    for num in numbers:
-        if abs(num) > res and -num in helper:
-            res = abs(num)
-        else:
-            helper.add(num)
-    return res if res else -1
-
-#  N ^ 2 implementation
-
-
-def find_sums(arr, target):
-    result = False
-    if not arr or len(arr) < 2:
-        return False
-    for i, num in enumerate(arr):
-        complement = target - num
-        # Проверяем что complement есть в оставшейся части массива
-        if complement in arr[i+1:]:
-            result = True
-            break
-
-    return result
-
-
-
-
+def getNumbersRange(lst: list[int], min: int, max: int) -> list[int]:
+    res: list[int] = []
+    for num in lst:
+        if min <= num <= max:
+            res.append(num)
+    return res        
+    
+print(getNumbersRange(numbers, 25, 40))   
