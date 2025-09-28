5402dbdb73dfc05294760b573c061461a9eb885b
Yuragranovsky <yuragranovsky@gmail.com>
2025-09-28 21:31:03 +0300

    Person, Club classes, most effective implementation

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
-from sortedcontainers import SortedKeyList
-# used for lists/arrays with  frequent add/remove operaions
+from dataclasses import dataclass, field
+from sortedcontainers import SortedSet, SortedKeyList
 
-# method add has complexity O[LogN]
-sortedList: SortedKeyList[str] = SortedKeyList(key=str.casefold)
-sortedList.add("AbCd")
-sortedList.add("aBc")
-sortedList.add("abcd")
-sortedList.add("ac")
-
-
-print(sortedList)
+@dataclass(order=True, unsafe_hash=True)
+class Person:
+    id: int
+    age: int = field(compare = False)
     
+prs1: Person = Person(123, 25)
+prs2: Person = Person(100, 40) 
+prs3: Person = Person(50, 40)
+print(f"prs1 > prs2 is {prs1 > prs2}")
+print (f"{prs3} == Person(50, None) is {prs3 == Person(50, None)}")
+class Club: 
+    def __init__(self):
+        self.__sortedSet = SortedSet()
+        self.__sortedKeyList = SortedKeyList(key=lambda p: (p.age, p.id))
+    def addPerson(self, person: Person):
+        # adds person
+        # raises ValueError if person already exists
+        if person in self.__sortedSet:
+            raise ValueError(f"person with id {person.id} already exists")
+        self.__sortedSet.add(person)
+        self.__sortedKeyList.add(person)
+    def getAllSortedId(self)-> list[Person]:
+        return list(self.__sortedSet)
+    def getAllSortedAgeId(self)->list[Person]:
+        return list(self.__sortedKeyList)
+    
+club: Club = Club()
+club.addPerson(prs1)
+club.addPerson(prs2)
+club.addPerson(prs3)
+print("sorted by id -> ", club.getAllSortedId())
+print("sorted by age, id -> ", club.getAllSortedAgeId())
+
+
+
