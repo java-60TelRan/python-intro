83e93bd550a07e247369d014b3a2479d6698d377
Yuragranovsky <yuragranovsky@gmail.com>
2025-09-28 20:18:06 +0300

    SortedKeyList for strings insensitive comparing

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-from sortedcontainers import SortedSet
+from sortedcontainers import SortedKeyList
 # used for lists/arrays with  frequent add/remove operaions
 
 # method add has complexity O[LogN]
-sortedList: SortedSet[int] = SortedSet()
-sortedList.add(30)
-sortedList.add(20)
-sortedList.add(50)
-sortedList.add(50) # no added in SortedSet as duplications are diallowed
-sortedList.add(3)
+sortedList: SortedKeyList[str] = SortedKeyList(key=str.casefold)
+sortedList.add("AbCd")
+sortedList.add("aBc")
+sortedList.add("abcd")
+sortedList.add("ac")
 
 
-def getNumbersSortedRange(lst: list[int], min: int, max: int) -> list[int]:
-#    return part of list with closed range [min-max]
-   left: int = sortedList.bisect_left(min)
-   right: int = sortedList.bisect_right( max) 
-   return lst[left:right] 
+print(sortedList)
     
-print(getNumbersSortedRange(sortedList, 25, 50))   
