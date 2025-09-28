7877bb961f7d2431a6892847eb7fe170b48b9828
Yuragranovsky <yuragranovsky@gmail.com>
2025-09-28 19:28:35 +0300

    using bisect

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+import bisect
+# used for lists/arrays with not frequent add/remove operaions
 numbers:list[int] = []
-numbers.append(30)
-numbers.append(20)
-numbers.append(50)
-numbers.append(3)
+# insort has complexity O[N]
+bisect.insort(numbers,30)
+bisect.insort(numbers,20)
+bisect.insort(numbers,50)
+bisect.insort(numbers,50)
+bisect.insort(numbers,3)
 
-def getNumbersRange(lst: list[int], min: int, max: int) -> list[int]:
-    res: list[int] = []
-    for num in lst:
-        if min <= num <= max:
-            res.append(num)
-    return res        
+
+def getNumbersSortedRange(lst: list[int], min: int, max: int) -> list[int]:
+#    return part of list with closed range [min-max]
+   left: int = bisect.bisect_left(lst, min)
+   right: int = bisect.bisect_right(lst, max) 
+   return lst[left:right] 
     
-print(getNumbersRange(numbers, 25, 40))   
+print(getNumbersSortedRange(numbers, 25, 50))   
