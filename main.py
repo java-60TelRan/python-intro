b780098a4de988275698e1d3c8e5feb29c69e93c
Yuragranovsky <yuragranovsky@gmail.com>
2025-10-15 20:52:17 +0300

    Iterable demo CW #25

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# stack defines access to only last element LIFO
-# regular list / array workd fine for stack
-# adding / removing last element is performed with complexity O[1]
-# stack: list[int] = [1, 2, 10, -5]
-# stack.append(10) #adds at ene of stack
-# stack.pop() #removes last element
-# print(stack)
-################################################
-# queue define access for adding at end and for removing from beginning, FIFO (First In First Out) 
-# queue: list[int]= [1, 2, 10, -5] not good choice - because removing first item is performed with complexity O[N]
-# from collections import deque
-# queue: deque[int] = deque([1, 2, 10, -5]) # right choice for queue because removing first item is performed with complexity O[1]
-# queue.append(10)
-# queue.popleft() # complexity O[1]
-# print(queue)
-
+from dataclasses import dataclass
+from typing import Iterator
+@dataclass
+class Employee:
+    id: int
+    name: str
+    
+class EmployeeRepo:
+    def __init__(self, employees: list[Employee]):
+        self.__employees = employees.copy()
+    def __iter__(self) -> Iterator[Employee]:
+        return iter(self.__employees)    
+        
+repo: EmployeeRepo = EmployeeRepo([Employee(1243, "Vasya"), Employee(1245, "Petya")])
+names: list[str] = [e.name for e in repo] 
+print(names)      
