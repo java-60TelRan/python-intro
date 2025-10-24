0b2c4d80ebbab2e5518517cfa1a7b920a0c71d14
Yuragranovsky <yuragranovsky@gmail.com>
2025-10-24 14:14:04 +0300

    HW #27 LFU Cache

--- main.py
+++ main.py
@@ -31,23 +31,56 @@ class DictCache(OrderedDict[K, V]) :
         
 class  LfuDictCache(Generic[K, V]):
     def __init__(self, max_size: int):
-        #TODO write constructor for defining encapsulated data structure
-        raise NotImplementedError()
+        self.max_size = max_size
+        self.cache: dict[K, tuple[V, int]] = {} #dictionary with key as K and value as tuple (V, <frequency as number of accesses>)
+        self.frequency_key:SortedDict[int, DictCache[K, None]] = SortedDict() #sorted dictionary with key as frequency and value as LRU cache (DictCache[K, V])
     def __getitem__(self, key: K) -> V:
-        #TODO method for square braces operator [] getting key and returnin value with throwing
-        #KeyError exception if key is missing
-        raise NotImplementedError()
-    def __setitem__(self, key: K, value: V):
-        # TODO method for square braces operator [] either updating existing key-value association or adding a new one
+        value, frequency = self.cache[key] # unpacking of tuple with value and frequency
+        self.__update_cache(key, value, frequency)
+        return value
+
+    def __update_cache(self, key, value, frequency):
+        # removing from lru_cache as value of dictionary frequency-DictCache association with a given key for frequency
+        # and adding the key to another lru_cache for the incremented frequency (frequency + 1)
+        new_frequency = frequency + 1
+        self.cache[key] = (value, new_frequency)
+        if frequency >= 0:
+            lru_cache: DictCache[K, None] = self.frequency_key[frequency]
+            lru_cache.pop(key)
+            if not lru_cache:
+                # remooving association from sorted dictionary if LRU cache is empty
+                self.frequency_key.pop(frequency)
+        self.frequency_key.setdefault(new_frequency, DictCache(self.max_size))[key] = None
         
-        raise NotImplementedError() 
+    def __setitem__(self, key: K, new_value: V):
+        frequency: int = -1
+        if key in self.cache:
+            # no adding new association 
+           frequency = self.cache[key][1]
+        elif len(self.cache) == self.max_size:
+            # no room for new association - deleting LFU
+            self.__lfu_delete()
+        self.cache[key] = (new_value, frequency + 1)
+        self.__update_cache(key, new_value, frequency)
+    def __lfu_delete(self):
+        frequency: int
+        lru_cache: DictCache[K, None]
+        frequency, lru_cache = self.frequency_key.peekitem(0)
+        key_to_delete: K = lru_cache.popitem(last = False)[0] #remooving LRU 
+        if not lru_cache:
+            # remooving association from sorted dictionary if LRU cache is empty
+            self.frequency_key.pop(frequency)
+        self.cache.pop(key_to_delete) # removing from cache dictionary   
+                  
+           
     def __delitem__(self, key: K):
-        # TODO method for deleting key-value association from a dictionary with throwing KeyError exception
-        # in the case of missing key like del dict[key] 
-        raise NotImplementedError()
+        frequency = self.cache.pop(key)[1] # removing from cache dictionary
+        lru_cache: DictCache[K, None] = self.frequency_key[frequency] 
+        del lru_cache[key] #removing from LRU
+        if not lru_cache:
+            del self.frequency_key[frequency] # remooving association from sorted dictionary if LRU cache is empty
+        
     def __iter__(self) -> Iterator[K]:
-        # TODO method for iterating keys in arbitrary order 
-        raise NotImplementedError()
+        return iter(self.cache)
     def __len__(self)->int:
-        # TODO method returning number of key-value associations (pairs)
-        raise NotImplementedError()
+       return len(self.cache)
