f4ad53bd879a492bade3a0fc5f8e39abb9b525ba
Yuragranovsky <yuragranovsky@gmail.com>
2025-11-21 16:05:03 +0200

    HW #35 solution

--- regular_expressions.py
+++ regular_expressions.py
@@ -2,14 +2,16 @@ def pythonicNameRe()->str:
     return r"[A-Za-z_]\w*"
 def passwordRe():
     return r"(?=.*[A-Z])(?=.*[a-z])(?=.*[#$%])(?=.*[\d])[A-Za-z#$%\d_-]{8,}"
+def __ipV4Octet()-> str:
+    return r"([0-1]?\d{1,2}|2([0-4]\d|5[0-5]))"
 def ipV4AddressRe()->str:
-    # TODO
     """returns regexp as match pattern of IPv4 address
        comprises of 4 octets separated by dot
        each octet contains 1-3 symbols from 0 to 255
-    """    
+    """ 
+    octet_grp = __ipV4Octet()  
+    return rf"({octet_grp}\.){{3}}{octet_grp}" 
 def mobileIsraelNumberRe()->str:
-    # TODO
     """returns regexp for mobile phone Israel number
        +972- - Israel preffix (not mandatary)
        Operator preffix 0 (only without +972-)
@@ -19,4 +21,5 @@ def mobileIsraelNumberRe()->str:
        xxxxxxx
        xxx-xx-xx
        x-xx-xx-xx
-    """    
+    """  
+    return r"(\+972-?|0)5\d-?\d-?\d{2}-?\d{2}-?\d{2}"
