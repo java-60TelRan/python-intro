b8e1e138007392e2e342f8f5a136d73b0b731fb1
Yuragranovsky <yuragranovsky@gmail.com>
2025-11-23 19:08:26 +0200

    extraction of data using finditer

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import regular_expressions as regex
+import sys
 import re
 text: str = "The internal network contains the following devices.\
     WEB server is running at 172.0.5.20. Database server is running at 10.0.5.20.\
         Backup is available through 172.0.5.40. Monitoring sends notifications from 85.12.30.7.\
             Whitelist for access from external network contains IP addresses from 200.13.4.1 to \
                 200.13.4.100"
-ip_regex= regex.ipV4AddressRe()               
-mo:re.Match = re.search(ip_regex, text)
-print(f"IP address: {mo.group()}; start index: {mo.start()}; end index: {mo.end()}")                
+ip_regex= regex.ipV4AddressRe()  
+devices = ["WEB Server","DB server", "Backup", "Monitoring", "Whitelist From", "Whitelist To"]
+devAddresses = {devices[i]: mo.group() for i, mo in enumerate(re.finditer(ip_regex, text))}
+print("all IP address of the known devices ", devAddresses)
+print("IP address of WEB server is ", devAddresses["WEB Server"])
